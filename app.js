92220c1898ee3668c6beda58118ca40bcaff3a78
89apt89 <89apt89@protonmail.ch>
2019-02-23 14:01:24 -0500

    fix undefined forecast

--- app.js
+++ app.js
@@ -12,37 +12,40 @@ app.get('/', (req, res) => {
         weather.find({search: req.param('loc'), degreeType: 'C'}, (err, result) => {
             if (err) return console.log(err)
             today = new Date().toLocaleDateString("en", { weekday: 'long' })     
-
-            result[0].forecast.forEach(day => {
-                if (today.toLowerCase() == day.day.toLowerCase()) {
-                    loc = result[0].location
-                    cur = result[0].current
-
-                    console.log("Predicting for today...")
-                    perc = 0
-                    if (day.skytextday.toLowerCase().indexOf("snow") != -1) {
-                        perc = perc + 1
-                        if (day.precip) {
-                            perc = perc + 3
-                            perc = perc + Number(day.precip) / 4
-                        }
-                    } else {
-                        if (day.precip) {
+            if (result[0].forecast === undefined) {
+                res.json({error: "Location not found."})
+            } else {
+                result[0].forecast.forEach(day => {
+                    if (today.toLowerCase() == day.day.toLowerCase()) {
+                        loc = result[0].location
+                        cur = result[0].current
+                    
+                        console.log("Predicting for today...")
+                        perc = 0
+                        if (day.skytextday.toLowerCase().indexOf("snow") != -1) {
                             perc = perc + 1
-                            perc = perc + Number(day.precip) / 10
+                            if (day.precip) {
+                                perc = perc + 3
+                                perc = perc + Number(day.precip) / 4
+                            }
+                        } else {
+                            if (day.precip) {
+                                perc = perc + 1
+                                perc = perc + Number(day.precip) / 10
+                            }
                         }
+                        if (Number(day.low) < 0) {
+                            low = Number(day.low).
+                            perc = perc + 5
+                            perc = perc + Number(day.low) / 5
+                        }
+                        if (loc.alert) {
+                            perc = perc + 15
+                        }
+                        res.json({chance: Math.abs(perc)})
                     }
-                    if (Number(day.low) < 0) {
-                        low = Number(day.low).
-                        perc = perc + 5
-                        perc = perc + Number(day.low) / 5
-                    }
-                    if (loc.alert) {
-                        perc = perc + 15
-                    }
-                    res.json({chance: Math.abs(perc)})
-                }
-            })
+                })
+            }
         })
     }
 })
