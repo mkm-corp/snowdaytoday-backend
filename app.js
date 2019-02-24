c3df834a4ae717f35b152e3bdfc093741b1681c4
89apt89 <89apt89@protonmail.ch>
2019-02-24 00:48:08 -0500

    fi more

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ app.get('/:place', (req, res) => {
     weather.find({search: place, degreeType: 'C'}, (err, result) => {
         if (err) return console.log(err)
         if (typeof result[0] === "undefined") {
-            res.json({error: "not found"})
+            res.json({error: "not found", date: new Date(), chance: 0})
             return
         }
         today = new Date().setHours(0,0,0,0)
         result[0].forecast.forEach(day => {
             date = new Date(day.date)
-            //console.log(new Date(date-today))
             if (new Date(date-today).valueOf() == new Date("1970-01-01T19:00:00.000Z").valueOf()) {
                 /*if (day.date.toLowerCase() == "sunday" || day.date.toLowerCase() == "saturday") {
                     res.json({error: "weekend", date: date})
@@ -46,67 +45,14 @@ app.get('/:place', (req, res) => {
                 if (loc.alert) {
                     perc = perc + 15
                 }
-                res.json({chance: (Math.abs(perc)).toFixed(2), date: day.date})
+                res.json({chance: (Math.abs(perc)).toFixed(2), date: day.date, error: null})
             }
         })
     })
 })
-/*app.get('/', (req, res) => {
-    if (req.param('loc', undefined) === undefined) {
-        res.json({error: "Location not found."})
-    } else {
-        var weather = require('weather-js');
-        weather.find({search: req.param('loc'), degreeType: 'C'}, (err, result) => {
-            if (err) return console.log(err)
-            t = new Date()
-            today = new Date(t.getFullYear()+"-"+(t.getMonth()+1)+"-"+t.getDate())
-            if (typeof result[0] === "undefined") {
-                res.json({error: "Location not found."})
-            } else {
-                result[0].forecast.forEach(day => {
-                    date = new Date(day.date)
-                    if (date-today==1) {
-                        console.log("Oh? Bruh?")
-                    }
-                })
-                /*result[0].forecast.forEach(day => {
-                    if (today.toLowerCase() == day.day.toLowerCase()) {
-                        console.log(day)
-                        if (today.toLowerCase() == "sunday" || today.toLowerCase() == "saturday") {
-                            res.json({error: "weekend", date: day.date})
-                            return
-                        }
-                        loc = result[0].location
-                        cur = result[0].current
 
-                        console.log("Predicting for today...")
-                        perc = 0
-                        if (day.skytextday.toLowerCase().indexOf("snow") != -1) {
-                            perc = perc + 1
-                            if (day.precip) {
-                                perc = perc + 3
-                                perc = perc + Number(day.precip) / 4
-                            }
-                        } else {
-                            if (day.precip) {
-                                perc = perc + 1
-                                perc = perc + Number(day.precip) / 10
-                            }
-                        }
-                        if (Number(day.low) < 0) {
-                            low = Number(day.low).
-                            perc = perc + 5
-                            perc = perc + Number(day.low) / 5
-                        }
-                        if (loc.alert) {
-                            perc = perc + 15
-                        }
-                        res.json({chance: Math.abs(perc), date: day.date})
-                    }
-                })
-            }
-        })
-    }
-})*/
+app.get('/', (req, res) => {
+    res.json({error: "not found", date: new Date(), chance: 0})
+})
 
 app.listen(port, "127.0.0.1")
